d12ccd6533ba7873fb036c0af180ac3cf3677799
Ilija Radovanović <ilija009@gmail.com>
2022-01-14 00:15:08 +0100

    fix

--- scripts/muffins.js
+++ scripts/muffins.js
@@ -61,7 +61,7 @@ function changeSlide(direction) {
 window.addEventListener("load",function() {
     setTimeout(function(){
         // This hides the address bar:
-        window.scrollTo(0, 1);
+        window.scrollTo(0, window.innerHeight);
     }, 0);
 });
 
